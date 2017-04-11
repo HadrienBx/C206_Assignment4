ea50183b38e136927443929d142b70439f25e16b
hadrien blampoix <hadrien blampoix>
2017-04-11 13:50:16 -0400

    updates

---
+++ success.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys
+import cgi
+import csv
+import math
+import urllib2
+import ctypes
+
+def success():
+
+	f = open('resources.csv', 'rb')
+	reader = csv.reader(f)
+	for row in reader:
+		rdata = row
+	f.close()
+	rmana = rdata[0]
+	rgp = rdata[1]
+	rocc = rdata[2]
+
+	f = open('resources.csv', 'r+')
+	f.seek(0)
+	f.write(rmana)
+	f.write(',')
+	f.write(rgp)
+	f.write(',0')
+	#f.truncate()
+	f.close()
+
+success()
--- transporter.py
+++ transporter.py
@@ -1,88 +1,82 @@
-#!/usr/bin/env python
-import math
+#!/usr/bin/python
+
+import sys
 import cgi
 import csv
+import math
 import urllib2
 import ctypes
 
-def transporter (player_inventory, url):
-	
-	# get and reads csv, check if the number on index 2 is 0 or 1
-	f = open("resources.csv","r")
-	csv_f = csv.reader(f) 
-	form = cgi.FieldStorage()
-	url = form.getvalue('url')
-	user_inventory = form.getvalue('inventory')
-	value = user_inventory.split(",")
-	manna = value[0]
-	gold = value[1]
-	for line in csv_f:
-		room_status = line[2]
+def transporter():
 
-		# if room is occupied
-		if (room_status == 1):
-			print "Content-type: text/html\r\n"
- 			print "<html>"
- 			print '<body align = "center">'
-			print '<p> The room you tried to enter is occupied!</br>'
- 			print "Manna is %s </br>" %(manna)
-			
-			#TODO jesus christ i hope this works
- 			print '<form name = "return" action = "' + url +'"' + ' method ="post">'
-			print '<input type = "hidden" name = "inventory" value ="' + manna + ',' + gold + '"
-			print '<input type ="submit" value = "return">
-			print '</form>'
-			print "</p>
- 			print "</body>"
- 			print "</html>" 
-
-			# if number is 1, calls the url and goes back to prev room
-			# response = urllib2.urlopen(url)
-			# html = response.read() 
-
-			# change occupancy field by changing csv field to 0
-			
-			# fo = open ('resources.csv', 'w+')
-
-		elif (room_status == 0): #room is available
-			 
-			 print "Content-Type: text/html\n\n";
-			 print "<html>";
-			 print "\t<head>";
-			 print "\t\t<title>Room Name</title>";
-			 print "\t</head>";
-			 print "\t<body>";
-			 print "\t\t<img src=\"penthouse.jpg\" align=\"top\" width=\"1263\" height=\"500\" />";
-			 print "\t\t<form name=\"textbox\" action=\"https://www.cs.mcgill.ca/~hblamp/cgi-bin/room.cgi\" method=\"get\" >");
-			 print "\t\t\t<input type=\"text\" name=\"command\">";
-			 print "\t\t\t<input type=\"hidden\" name=\"inventory\" value=\"%d,%d\">", pmana, pgp;
-			 print "\t\t\t<input type=\"submit\" value=\"Submit\">";
-			 
-			 print "\t\t</form>";
-			 print "\t\t<p>%s.</p>",command
-			 print "\t\t<p>%d</p>",pmana
-			 print "\t\t<p>%d</p>",pgp
-			 print "\t\t<p>%d</p>",dropgp
-			 print "\t\t<p>%d</p>",rmana
-			 print "\t\t<p>%d</p>",rgp
-			 print "\t\t<p>%d</p>",rocc
-			 print "\t</body>"
-			 print "</html>"
-
-			# changing their occupancy field in their resources.csv
-
-
-			f2 = open(their resources.cv url)
-			csv_f2 = csv.reader(csv_f2)
-			fileout = open ('resources.csv', 'w+')
-			for line in f:
-				values = line.split(',')
-				token[2] = '"0"'
-				fo.write(','.join(token))
-			f.close()
-			fo.close()
-
-		
-
+	#read from csv
+	f = open('resources.csv', 'rb')
+	reader = csv.reader(f)
+	for row in reader:
+		rdata = row
 	f.close()
-	fo.close()
+	rmana = rdata[0]
+	rgp = rdata[1]
+	rocc = rdata[2]
+
+	#read from form
+	form = cgi.FieldStorage()
+	if form.getvalue("url"):
+		url = form.getvalue("url")
+	if form.getvalue("inventory"):
+		pinv = form.getvalue("inventory")
+	valuser = pinv.split(",")
+	pmana = valuser[0]
+	pgp = valuser[1]
+
+	#print
+	print "Content-type: text/html\n\n"
+	print "<html>"
+	print "<head>"
+	print "<title> Teleporting </title>"
+	print "</head>"
+	print "<body>"
+	print "</br></br></br></br></br>"
+	if int(rocc) == 0 and int(pmana) >= 1:
+		#output = int(rmana),int(rgp),1
+		#print "<p>", output, "</p>"
+		#print "<p>", rmana,",",rgp,",1", "</p>"
+		#int(pmana) -= 1
+		#int(rmana) += 1
+		if int(pmana) == 0:
+			# => room.cgi => registers pmana=0 => game over screen
+			print "<h1 align=\"center\"> Out of Manna! Game Over! </h1></br></br></br>"
+			print "<form name=\"gameover\" action=\"http://www.cs.mcgill.ca/~hblamp/cgi-bin/room.cgi\" method=\"get\" align=\"center\" >"
+			print "<input type=\"hidden\" name=\"command\" value=\"REFRESH\" >"
+			print "<input type=\"hidden\" name=\"inventory\" value=\"0,10\" >"
+			print "<input type=\"submit\" value=\"Click to Exit!\" >"
+			print "</br></br></br>"
+		else:
+			#enter room!
+			#MUST CALL success.py somehow!
+			f = open('resources.csv', 'r+')
+			f.seek(0)
+			f.write(rmana)
+			f.write(',')
+			f.write(rgp)
+			f.write(',1')
+			#f.truncate()
+			f.close()
+			print "<h1 align=\"center\"> Teleport Successful! </h1></br></br></br>"
+			print "<form name=\"EnterRoom\" action=\"http://www.cs.mcgill.ca/~hblamp/cgi-bin/room.cgi\" method=\"get\" align=\"center\"  >"
+			print "<input type=\"hidden\" name=\"command\" value=\"REFRESH\" >"
+			print "<input type=\"hidden\" name=\"inventory\" value=\"20,20\" >"
+			print "<input type=\"submit\" value=\"Click to Enter Room!\" >"
+			print "</br></br></br>"
+	else:
+		print "<h1 align=\"center\"> Room Full! </h1></br></br></br>"
+		print "<form name=\"roomfull\" action=\"http://www.cs.mcgill.ca/~hblamp/cgi-bin/room.cgi\" method=\"get\" align=\"center\" >"
+		print "<input type=\"hidden\" name=\"command\" value=\"REFRESH\" >"
+		print "<input type=\"hidden\" name=\"inventory\" value=\"10,10\" >"
+		print "<input type=\"submit\" value=\"Click to Go Back!\" >"
+		print "</br></br></br>"
+	print "</br></br></br></br></br></br></br><p>rmana= ", rmana, " rgp= ", rgp, " rocc= ", rocc, "</br> url= ", url, " pmana= ", pmana, " pgp", pgp, "</p>"
+	print "</body>"
+	print "</html>"
+
+transporter()
